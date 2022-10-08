e6b9a2aed601f2f1556756c71990c0b51f26a843
Kledson Basso <kledsonhugo@gmail.com>
2022-10-08 11:23:10 -0300

    Terraform Provider update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,18 +1,24 @@
 # PROVIDER
 terraform {
 
+  required_version = "~> 1.3.1"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 4.34"
     }
   }
 
   backend "s3" {
     bucket         = "tf-state-vpcpeering-bucket"
     key            = "terraform.tfstate"
-    region         = "us-east-1"
     dynamodb_table = "tf-state-vpcpeering-table"
+    region         = "us-east-1"
   }
 
+}
+
+provider "aws" {
+  region  = "us-east-1"
 }
