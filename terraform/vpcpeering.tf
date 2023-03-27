0627555a1d58184749131f920304a41e828ab9d1
Kledson Basso <kledsonhugo@gmail.com>
2023-03-27 02:01:52 -0300

    README updates

--- terraform/vpcpeering.tf
+++ terraform/vpcpeering.tf
@@ -17,9 +17,10 @@ resource "aws_vpc" "vpc20" {
 
 # SUBNETS
 resource "aws_subnet" "sn_vpc10_pub" {
-    vpc_id            = aws_vpc.vpc10.id
-    cidr_block        = "10.0.1.0/24"
-    availability_zone = "us-east-1a"
+    vpc_id                  = aws_vpc.vpc10.id
+    cidr_block              = "10.0.1.0/24"
+    availability_zone       = "us-east-1a"
+    map_public_ip_on_launch = true
     tags = {
         Name = "sn_vpc10"
     }
